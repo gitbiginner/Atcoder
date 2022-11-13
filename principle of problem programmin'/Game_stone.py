4a77622375e3e9901e5468be87623371a20669ef
gitbiginner <brackhorn@gmail.com>
2022-11-13 21:10:07 +0900

    game

---
+++ principle of problem programmin'/Game_stone.py
@@ -0,0 +1,19 @@
+N, A, B = map(int, input().split())
+
+# dp[x] = True;  First win
+# dp[x] = False;  Second win
+
+dp = [None]*(N+1)
+
+for i in range(N+1):
+    if i>= A and dp[i-A]== False:
+        dp[i] = True
+    elif i>= B and dp[i-B]==False:
+        dp[i] = True
+    else:
+        dp[i] = False
+
+if dp[N]== True:
+    print('First')
+else:
+    print('Second')
