c3e3997028d74e1e9836d0e286515249b2bd6aa4
gitbiginner <brackhorn@gmail.com>
2022-12-20 21:16:51 +0900

    deque

---
+++ principle of problem programmin'/datastructure_query/Stack.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+Q = int(input())
+
+#queries = [input().split() for i in range(Q)]
+#queries = list(input().split() for i in range(Q))
+queries = [None]*Q
+for i in range(Q):
+    queries[i] = input().split()
+
+S = deque()
+
+for q in queries:
+    if q[0] == "1":
+        S.append(q[1])
+    if q[0] == "2":
+        print(S[-1])
+    if q[0] =="3":
+        S.pop()
