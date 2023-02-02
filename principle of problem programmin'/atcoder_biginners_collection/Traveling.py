5a2e6a0c26d25c1995170cba1693538556bb5b23
gitbiginner <brackhorn@gmail.com>
2023-02-02 21:57:54 +0900

    2darray is not error

--- principle of problem programmin'/atcoder_biginners_collection/Traveling.py
+++ principle of problem programmin'/atcoder_biginners_collection/Traveling.py
@@ -1,29 +1,24 @@
 # https://atcoder.jp/contests/abs/tasks/arc089_a
 
 
-N=int(input())
- 
-t,x,y=0,0,0
- 
-for _ in range(N):
- 
-  T,X,Y=map(int, input().split())
- 
-  l=abs(x-X)+abs(y-Y)
- 
-  z=T-t-l
- 
-  if z>=0 and z%2==0:
- 
-    t,x,y=T,X,Y
- 
-    continue
- 
-  else:
- 
-    print("No")
- 
-    exit()
- 
-print("Yes")
-
+N = int(input())
+X = []
+x,y,t = 0,0,0
+for i in range(N):
+    X.append(list(map(int, input().split())))
+possibility = True
+for j in range(N):
+    if X[j][0] % 2 == (X[j][1] + X[j][2]) % 2:
+        if X[j][0] - t < abs(X[j][1] - x) + abs(X[j][2] - y):
+            possibility = False
+        t = X[j][0]
+        x = X[j][1]
+        y = X[j][2]
+    else:
+        possibility = False
+        break
+    
+if possibility:
+    print('Yes')
+else:
+    print('No')
