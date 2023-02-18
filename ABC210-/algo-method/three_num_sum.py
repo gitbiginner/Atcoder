004e6b90bdad0cb17b02638436739e0597794703
gitbiginner <brackhorn@gmail.com>
2023-02-18 23:12:20 +0900

    faster

--- ABC210-/algo-method/three_num_sum.py
+++ ABC210-/algo-method/three_num_sum.py
@@ -1,10 +1,18 @@
 
 # https://algo-method.com/tasks/1024/editorial
 
+
+N, M = map(int, input().split())
+
 ans = 0
-for x in range(1, N + 1):
-    for y in range(1, N + 1):
-        for z in range(1, N + 1):
-            if x + y + z <= M:
-                ans += 1
-#rrr
+
+
+for x in range(1, N+1):
+    for y in range(1, N+1):
+        max_z = min(N, M-x-y)
+        if max_z <= 0:
+            continue
+        else:
+            ans += max_z
+
+print(ans)
