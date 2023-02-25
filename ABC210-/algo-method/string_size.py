fad6741a96687a4609c5f342e241c34c197a3643
gitbiginner <brackhorn@gmail.com>
2023-02-25 23:25:18 +0900

    algo size 10**5+2

--- ABC210-/algo-method/string_size.py
+++ ABC210-/algo-method/string_size.py
@@ -5,9 +5,23 @@ N= int(input())
 L = list(map(int, input().split()))
 
 
+count = [0]*(10**5+1) # 10**5
 
+for i in L:
+    count[i] += 1 
+
+acc = [0]*(10**5+2) # 10**5 +1
+
+
+for ind, cnt in enumerate(count):
+    acc[ind+1] = acc[ind]+cnt
 
 
 Q = int(input())
+
+
+for _ in range(Q):
+    A, B = map(int, input().split())
+    print(acc[B+1]-acc[A])
 
 
