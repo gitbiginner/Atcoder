7b65bbf5cd93dfa6b5a5bc905f5c8fc6e17f84c1
gitbiginner <brackhorn@gmail.com>
2023-03-04 19:48:54 +0900

    hours to minuites

--- ABC210-/algo-method/culc/work_hours.py
+++ ABC210-/algo-method/culc/work_hours.py
@@ -1,1 +1,17 @@
 # 
+# https://algo-method.com/tasks/1093
+
+ans = 0
+for i in range(30):
+    H1, M1, H2, M2 = map(int, input().split())
+    minuite = 60*(H2-H1)+M2-M1
+    if H1==M1==H2==M2==0:
+        continue;
+    elif 360 < minuite <= 480:
+        minuite= minuite-45
+    elif minuite > 480:
+        minuite= minuite-60 
+
+    ans += minuite
+
+print(ans//60, ans%60)
